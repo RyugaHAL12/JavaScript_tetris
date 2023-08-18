e085f2508b30b7d5389471cec1eefd64f7a1ecac
ryugahal12 <ryugahal12@gmail.com>
2023-08-18 13:46:17 +0900

    テトリス途中。写経しているけどいろいろまとめれないかな～ってやっているうちにそういえばCanvasの四角動かせてないじゃんってことに気づいてブランチ切る。

--- Tetris/script/mainScript.js
+++ Tetris/script/mainScript.js
@@ -1,6 +1,6 @@
 //--定数--//
 //	ステージ系
-const Stage = {
+const StageSetting = {
 	BlockSize : 24,		//	1ブロックのサイズ
 	StageRows : 22,		//	ステージの高さ(20ラインをステージとし、上下1ラインを当たり判定とブロックコピー用)
 	StageCols : 12,		//	ステージの幅
@@ -18,6 +18,7 @@ const GameStatus = {
 	Effect : 2,
 };
 
+//	ブロックの状態
 const BlockStatus = {
 	Non : 0,		//	存在しない
 	Normal : 1,		//	通常時(動かせる)
@@ -26,10 +27,11 @@ const BlockStatus = {
 	Wall : 9,		//	壁
 };
 
+//	色
 const Colors = {
-	Background : "#ddd",		//	背景色
-	Gameover : "palevioletred",	//	ゲームオーバー時
-	Block : "steelblue",	//	操作ブロックの色
+	Background : "#ddd",			//	背景色
+	Gameover : "palevioletred",		//	ゲームオーバー時
+	Block : "steelblue",			//	操作ブロックの色
 	Lockedblock : "lightslategray",	//	ロックしたブロックの色
 	Error : "tomato",
 	Effect1 : "whitesmoke",
@@ -40,12 +42,55 @@ const Colors = {
 const NextLevel = 10;		//	次のレベルまでの消去ライン数
 
 //--Global--//
-let elmCanvas;						//	キャンバス要素
-let canvasContext;					//	キャンバスコンテキスト
-let stageFrame;						//	ゲームステージ枠(壁の情報のみ・固定)
-let fieldFrame;						//	ゲーム中のステージ枠とブロック表示用
-let blockSize;						//	ブロックサイズ	
-let fallSpeed;						//	落下速度
-let gameFrameNum;					//	ゲームフレーム番号
-let fallBlockType = new Array();	//	落下してくるブロックの種類
-let 
+class Canvas{
+	m_elmCanvas;
+	m_canvasContext;
+	
+	constructor(){
+		m_elmCanvas = document.getElementById('canvas');
+		m_canvasContext = m_elmCanavs.getContext("2d");
+	};
+}
+
+class Stage{
+	m_Flame;
+	m_Field;
+	constructor(){
+		m_Flame = new Array(StageSetting=>StageCols);
+		m_Field = new Array(StageSetting=>StageCols);
+	};
+}
+
+class Block{
+	m_Size;
+	m_Type;
+	m_Status;
+	m_X;
+	m_Y;
+
+	constructor(){
+	};
+}
+
+let elmCanvas;									//	キャンバス要素
+let canvasContext;								//	キャンバスコンテキスト
+let stageFrame = new Array(Stage=>StageCols);	//	ゲームステージ枠(壁の情報のみ・固定)
+let fieldFrame = new Array(Stage=>StageCols);	//	ゲーム中のステージ枠とブロック表示用
+let blockSize;									//	ブロックサイズ	
+let fallSpeed;									//	落下速度
+let gameFrameNum;								//	ゲームフレーム番号
+let fallBlockType = new Array();				//	落下してくるブロックの種類
+let opeBlock = new Array();						//	操作中のブロック
+
+
+function init(){
+	clearTimeout(timer1);
+	FPS = 30;
+	clearLine = 0;
+
+	//	キャンバスの設定
+	let canvas = new Canvas();
+	canvas.m_elmCanvas.width = StageSetting=>ScreenWidth;
+	canvas.m_elmCanvas.height = StageSetting=>ScreenHeight;
+	
+}
